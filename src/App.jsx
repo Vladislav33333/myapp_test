ad251dfe03074075f3a1fef45ab17612be4478b7
Vladislav Prikupenko <vladik.fol@yandex.ru>
2025-04-21 20:24:32 +0300

    21.04.25 - Prikupenko

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./components/ToggleTheme/ToggleTheme.css";
-import Testprops from "./components/Testprops";
+import ToolTemplate from "./components/ToolTemplate";
 import users from "./components/toolsData";
 import { useState } from "react";
 
@@ -14,21 +14,22 @@ const App = () => {
   const handleOkClick = () => {
     setEditMode(false);
   };
+
   return (
     <div className="App">
       <h1>Mill tools</h1>
       <div className="TestProps_container">
-        <Testprops
+        <ToolTemplate
           id={users.tool1.id}
           name={users.tool1.name}
           isEditing={editMode}
         />
-        <Testprops
+        <ToolTemplate
           id={users.tool2.id}
           name={users.tool2.name}
           isEditing={editMode}
         />
-        <Testprops
+        <ToolTemplate
           id={users.tool3.id}
           name={users.tool3.name}
           isEditing={editMode}
