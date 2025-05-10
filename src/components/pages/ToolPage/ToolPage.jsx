ad988d21c59e579465ff4f4b8ded392664428d4f
Vladislav33333 <vladik.fol@yandex.ru>
2025-05-10 23:57:17 +0300

    BorderLights OK

--- src/components/pages/ToolPage/ToolPage.jsx
+++ src/components/pages/ToolPage/ToolPage.jsx
@@ -1,14 +1,12 @@
 import { useState, useMemo, useCallback } from "react";
 import "./ToolPage.css";
-import { FixedSizeList as List } from "react-window";
 import { SearchBar } from "../../../components/pages/ToolPage/ToolPageSearchBar";
 import { ToolPageBackBtn } from "../../pages/ToolPage/ToolPageBackBtn";
 import { emptyRow } from "../../../services/emptyRow";
 import { categories } from "../../../services/toolsData";
 import ToolTemplate from "../ToolTemplate/ToolTemplate";
 import ToolPageButtons from "../ToolPage/ToolPageButtons";
-// import { Link } from "react-router-dom";
-// import { ZrTools } from "../ZrTools/ZrTools";
+
 export const ToolPage = ({ toolType = "mill" }) => {
   const [editMode, setEditMode] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
@@ -32,34 +30,28 @@ export const ToolPage = ({ toolType = "mill" }) => {
   const handleEditClick = useCallback(() => setEditMode(true), []);
   const handleOkClick = useCallback(() => setEditMode(false), []);
 
-  const Row = ({ index, style }) => {
-    const tool = filteredTools[index];
-    return (
-      <div style={style}>
-        <ToolTemplate id={tool.id} name={tool.name} isEditing={editMode} />
-      </div>
-    );
-  };
-
   return (
     <div className="ToolPage">
       <h1>{title}</h1>
-
       <div className="nav">
         <ToolPageBackBtn />
       </div>
-
       <SearchBar onClick={searchQuery} onChange={setSearchQuery} />
-
       <div className="list-container">
-        <List
-          height={460}
-          itemCount={filteredTools.length}
-          itemSize={110}
-          width="100%"
-        >
-          {filteredTools.length === 0 ? emptyRow : Row}
-        </List>
+        {filteredTools.length === 0 ? (
+          emptyRow
+        ) : (
+          <div className="tools-list">
+            {filteredTools.map((tool) => (
+              <ToolTemplate
+                key={tool.id}
+                id={tool.id}
+                name={tool.name}
+                isEditing={editMode}
+              />
+            ))}
+          </div>
+        )}
       </div>
       <ToolPageButtons {...{ editMode, handleEditClick, handleOkClick }} />
     </div>
