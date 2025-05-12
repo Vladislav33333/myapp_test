c63e7f9bc46ded2a5c91891fa4fc6ea45e00d4c2
Vladislav33333 <vladik.fol@yandex.ru>
2025-05-12 22:05:35 +0300

    12.05.25 - SmallFix

--- src/pages/ZrTools/ZrTools.jsx
+++ src/pages/ZrTools/ZrTools.jsx
@@ -3,14 +3,12 @@ import { Outlet, Link } from "react-router-dom";
 const ZrTools = () => {
   return (
     <div>
-      <nav className="nav">
-        <Link className="link" to="aiditeHT">
-          Aidite
-        </Link>{" "}
-        <Link className="link" to="upceraFunc">
-          Upcera
-        </Link>
-      </nav>
+      <Link className="link" to="aiditeHT">
+        Aidite
+      </Link>{" "}
+      <Link className="link" to="upceraFunc">
+        Upcera
+      </Link>
       <Outlet />
     </div>
   );
